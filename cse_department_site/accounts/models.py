d15bb98be09c59534f1c0738940a7e78b317488f
joydip <joydipd793@gmail.com>
2023-05-30 16:27:45 +0530

    add some other apps

--- cse_department_site/accounts/models.py
+++ cse_department_site/accounts/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # Add any additional fields if needed
+    pass
+class StudentProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    # Add student-specific fields
+    pass
+class StaffProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    # Add staff-specific fields
+    pass            
+class FacultyProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    # Add faculty-specific fields
+    pass
+class AlumniProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    # Add alumni-specific fields
+    pass
