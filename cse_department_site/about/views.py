532056b54f230a76c7a63db23d678abdd2dcf062
roshnidutta <129418474+roshnidutta@users.noreply.github.com>
2023-06-05 17:27:08 +0530

    roshni
    view.py

--- cse_department_site/about/views.py
+++ cse_department_site/about/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 from django . shortcuts import render
 from .models import Gallery
 
@@ -12,6 +10,6 @@ def carousel_images(request):
 
 def gallery_images(request):
     data = Gallery.object.all()
-    context = {'images:' data}
+    context = {'images': data}
     return render{request,'gallery.html', context} 
     
