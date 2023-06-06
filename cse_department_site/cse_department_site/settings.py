2aac38aee35d7782be929a822d7ac8ae4a1bc024
joydip <joydipd793@gmail.com>
2023-06-06 19:57:02 +0530

    add necery things to work

--- cse_department_site/cse_department_site/settings.py
+++ cse_department_site/cse_department_site/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'base',
     'accounts',
     'about',
@@ -72,6 +72,9 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
+    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
