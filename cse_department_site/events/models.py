dc740ff20a9f2394b00d3fdf857346f7186b28b5
swapnaaacodes <126751627+swapnaaacodes@users.noreply.github.com>
2023-06-06 11:02:27 +0530

    events

--- cse_department_site/events/models.py
+++ cse_department_site/events/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Notice(models.Model):
+    topic = models.CharField(max_length=255)
+    Slug = models.SlugField()
+    description = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class Events(models.Model):
+    topic = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
