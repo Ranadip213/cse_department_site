460a602861559cd26421535bf9068783f81befdb
joydip <joydipd793@gmail.com>
2023-06-17 20:33:46 +0530

    notice and events added

--- cse_department_site/base/views.py
+++ cse_department_site/base/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
 from about.models import Club
+from events.models import Notice, Events
 
 # Create your views here.
+
+
 def homePage(request):
     clubs = Club.objects.all()
-    context = {'clubs':clubs}
-    return render(request,'screens/home.html', context)
+    notices = Notice.objects.order_by('-date_added')
+    events = Events.objects.order_by('-date_added')
+    context = {'clubs': clubs,
+               'notices': notices,
+               'events': events}
+    return render(request, 'screens/home.html', context)
