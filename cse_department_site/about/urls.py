59a951c5e828433328fc7a4d1e5db1a88d688bf6
joydip <joydipd793@gmail.com>
2023-06-17 11:00:03 +0530

    added the club section to to bacvkend

--- cse_department_site/about/urls.py
+++ cse_department_site/about/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('clubs', views.clubes_page, name = 'clubs'),
     path('gallery', views.gallery_images, name = 'gallery'),
 ]
